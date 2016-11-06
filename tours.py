3e6bb260fc47a9a33ec29a06bbdd4e29d60675f2
Alexey Buistov <oleksii@backbase.com>
2016-11-06 21:03:00 +0200

    getting tour dates and locations

--- tours.py
+++ tours.py
@@ -1,25 +1,31 @@
 import sys, getopt
-import getpass
 import requests
-import time
 import lxml.html
 from lxml.cssselect import CSSSelector
-from collections import OrderedDict
 from termcolor import colored
-
-try:
-	from html import escape  # python 3.x
-except ImportError:
-	from cgi import escape  # python 2.x
-
-# Next navigation page
-has_next = CSSSelector('.next')
+from math import cos, asin, sqrt
 
 # On Tour Label
 on_tour = CSSSelector('.label')
 
 # Band link
 bands = CSSSelector('.grid-items-item-main-text > .link-block-target')
+
+# Concert City
+cities_css = CSSSelector('.events-list-item-venue--city')
+
+# Concert Country
+countries_css = CSSSelector('.events-list-item-venue--country')
+
+# Concert Dates xpath. Only select dates which have a specific city set
+date_xpath = """//*[@class='events-list']/tbody/
+tr[td[@class='events-list-item-venue']/div]
+/td[@class = 'events-list-item-art']/time"""
+
+# Next navigation page
+has_next = CSSSelector('.next')
+
+api_key = ''
 
 def main(argv):
 	print colored('TourFinder v0.1.0 (c) singulart@i.ua', 'yellow')
@@ -30,6 +36,14 @@ def main(argv):
 		opts, args = getopt.getopt(argv,"hs:",["style="])
 	except getopt.GetoptError:
 		usage()
+
+	try:
+		with open (".google-api-key", "r") as myfile:
+			api_key=myfile.read()
+			print api_key
+	except:
+		print colored('Unable to load Google API Key', 'red')
+
 	for opt, arg in opts:
 		if opt == '-h':
 			print 'tours.py -s <last.fm style tag>'
@@ -72,9 +86,20 @@ def main(argv):
 		if is_on_tour:
 			print colored('%s is on tour' %href, 'red')
 			now_on_tour.extend(href)
+			r = requests.get('http://www.last.fm%s/+events' % href)
+			tree = lxml.html.fromstring(r.text)
+			cities_results = cities_css(tree)
+			countries_results = countries_css(tree)
+			dates_results = tree.xpath(date_xpath)
+			if cities_css and countries_css:
+				for d in range(0, len(dates_results)):
+					print ' %s in %s, %s' %(dates_results[d].attrib['datetime'], cities_results[d].text, countries_results[d].text)
 
 
-
+def distance(lat1, lon1, lat2, lon2):
+	p = 0.017453292519943295
+	a = 0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
+	return 12742 * asin(sqrt(a))
 
 def usage():
 	print 'tours.py -s <last.fm style tag>'
