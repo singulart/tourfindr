2b40f883587b1bd3060be2eb49be7199fc9cca9c
Alexey Buistov <oleksii@backbase.com>
2016-11-07 14:26:33 +0200

    getting cities coordinates by calling Google API

--- tours.py
+++ tours.py
@@ -2,6 +2,10 @@ import sys, getopt
 import requests
 import lxml.html
 import json
+from urllib import unquote
+from datetime import date
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from lxml.cssselect import CSSSelector
 from termcolor import colored
 from math import cos, asin, sqrt
@@ -27,6 +31,10 @@ tr[td[@class='events-list-item-venue']/div]
 has_next = CSSSelector('.next')
 
 api_key = ''
+my_lat = 50.4501
+my_lng = 30.5234
+radius_km = 2000
+months_advance = 3
 
 
 def main(argv):
@@ -73,7 +81,6 @@ def main(argv):
 		band_results = bands(tree)
 
 		band_links.extend([result.get('href') for result in band_results])
-		print band_links[len(band_links) - 1]
 		nextel = has_next(tree)
 		if nextel:
 			page += 1
@@ -86,7 +93,7 @@ def main(argv):
 		tree = lxml.html.fromstring(r.text)
 		is_on_tour = on_tour(tree)
 		if is_on_tour:
-			print colored('%s is on tour' % href, 'red')
+			print colored('%s is on tour' % unquote(href.replace('/music/', '')), 'green')
 			now_on_tour.extend(href)
 			r = requests.get('http://www.last.fm%s/+events' % href)
 			tree = lxml.html.fromstring(r.text)
@@ -95,12 +102,21 @@ def main(argv):
 			dates_results = tree.xpath(date_xpath)
 			if cities_css and countries_css:
 				for d in range(0, len(dates_results)):
-					city_coords = json.loads(requests.get(
-						"https://maps.googleapis.com/maps/api/geocode/json?address=%s,%s&key=%s" % (
-						cities_results[d].text, countries_results[d].text, api_key)).text)
-					print ' %s in %s, %s (%s, %s)' % (
-					dates_results[d].attrib['datetime'], cities_results[d].text, countries_results[d].text,
-					city_coords['results'][0]['geometry']['location']['lat'], city_coords['results'][0]['geometry']['location']['lng'])
+					advance_period = date.today() + relativedelta(months =+ months_advance)
+					concert_date = datetime.strptime(dates_results[d].attrib['datetime'], '%Y-%m-%dT00:00:00').date()
+					if concert_date > advance_period:
+						# Access to Google API to get the coordinates of the city
+						city_coords = json.loads(requests.get(
+							"https://maps.googleapis.com/maps/api/geocode/json?address=%s,%s&key=%s" % (
+							cities_results[d].text, countries_results[d].text, api_key)).text)
+						city_lat = city_coords['results'][0]['geometry']['location']['lat']
+						city_lng = city_coords['results'][0]['geometry']['location']['lng']
+
+						# Filter by desired radius (in kilometres)
+						if distance(my_lat, my_lng, city_lat, city_lng) < radius_km:
+							print ' %s in %s, %s (%s, %s)' % (
+							dates_results[d].attrib['datetime'], cities_results[d].text, countries_results[d].text,
+							city_lat, city_lng)
 
 
 def distance(lat1, lon1, lat2, lon2):
@@ -110,7 +126,7 @@ def distance(lat1, lon1, lat2, lon2):
 
 
 def usage():
-	print 'tours.py -s <last.fm style tag>'
+	print 'python tours.py -s <last.fm style tag>'
 	sys.exit(2)
 
 
